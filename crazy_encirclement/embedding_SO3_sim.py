29e195ca284db06fcaadd3bd987c09cf243ae346
Dimitria Silveria <23ldy@queensu.ca>
2024-11-29 18:23:14 -0500

    final implementation

--- crazy_encirclement/embedding_SO3_sim.py
+++ crazy_encirclement/embedding_SO3_sim.py
@@ -1,10 +1,11 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-import quaternion
+import warnings
 import math
 from utils2 import R3_so3
 from scipy.linalg import expm
 from icecream import ic
+#the rotation is counterclockwise #################################
 class Embedding():
     def __init__(self,r,phi_dot,k_phi,tactic,n_agents,initial_pos,dt):
         self.phi_dot = phi_dot
@@ -42,6 +43,9 @@ class Embedding():
         target_r = np.zeros((3, self.n))
         target_v = np.zeros((3, self.n))
         
+        
+        unit = np.zeros((self.n, 3))
+
         unit = np.zeros((self.n, 3))
         if self.n >1:
             n_diff = int(np.math.factorial(self.n) / (math.factorial(2) * math.factorial(self.n-2)))
@@ -67,9 +71,8 @@ class Embedding():
 
             v_d_hat_x = np.array([-phi_dot_x, 0, 0])
             Rot_x = expm(R3_so3(v_d_hat_x.reshape(-1,1))*self.dt)
-            self.Rot_act[:,:,i] =self.Rot_act[:,:,i]@Rot_x# self.Rot_des[:,:,i].copy()#Rot_x@
-            #ic(np.rad2deg(phi -self.phi_new[i]))
-            #ic(self.Rot_act[:,:,i]-self.Rot_des[:,:,i])
+            self.Rot_act[:,:,i] =Rot_x@self.Rot_act[:,:,i]# self.Rot_des[:,:,i].copy()#Rot_x@
+
             pos_x = pos_rot[0]
             pos_y = pos_rot[1]
             #pos_x, pos_y, _ = pos_rot.parts[1:]  # Ignoring the scalar part
@@ -84,115 +87,84 @@ class Embedding():
             if self.n > 1:
                 phi_i = self.phi_cur[i]
                 if i == 0:
-                    phi_k = self.phi_cur[i+1]
-                    phi_j = self.phi_cur[self.n-1]
+                    phi_k = self.phi_cur[self.n-1] #ahead
+                    phi_j = self.phi_cur[i+1] #behind
                 elif i == self.n-1:
-                    phi_k = self.phi_cur[0]
-                    phi_j = self.phi_cur[i-1]
+                    phi_k = self.phi_cur[i-1]
+                    phi_j = self.phi_cur[0]
                 else:
-                    phi_k = self.phi_cur[i+1]
-                    phi_j = self.phi_cur[i-1]
+                    phi_k = self.phi_cur[i-1]
+                    phi_j = self.phi_cur[i+1]
 
-                wd = self.phi_dot_desired(phi_i, phi_j, phi_k, self.phi_dot, self.k_phi)
+
+                wd = self.phi_dot_desired(phi_i, phi_j, phi_k, self.phi_dot, self.k_phi,i)
             else:
                 wd = self.phi_dot
                 phi_i = self.phi_cur[0]
-
+            #wd = self.phi_dot
+            
             #first evolve the agent in phase
             v_d_hat_z = np.array([0, 0, -wd])
             x = self.r * np.cos(phi_i)
             y = self.r * np.sin(phi_i)
             Rot_z = expm(R3_so3(v_d_hat_z.reshape(-1,1))*self.dt)
             pos_d_hat = np.array([x, y, 0])
-            pos_d_hat = Rot_z@pos_d_hat.T#@Rot_z
+            pos_d_hat = Rot_z@pos_d_hat
             phi_d, _ = self.cart2pol(pos_d_hat)
-            ic(pos_d_hat,[x,y])
+
             phi_dot_x = np.cos(phi_d)*np.sin(phi_d)#*(phi_i-self.phi_new[i])
-            phi_dot_y = 0*self.scale*np.cos(phi_i)**2*np.sin(phi_i) #phi_i-phi_prev[i]*
+            phi_dot_y = 0*self.scale*np.cos(phi_d)**2*np.sin(phi_d) #phi_i-phi_prev[i]*
             v_d_hat_x = np.array([-phi_dot_x, 0, 0])
             
             Rot_x = expm(R3_so3(v_d_hat_x.reshape(-1,1))*self.dt)
             
             v_d_hat_y = np.array([0, -phi_dot_y, 0])
             Rot_y = expm(R3_so3(v_d_hat_y.reshape(-1,1))*self.dt)
-            # if self.initial_phase[i] ==0:
-            # if (self.phi_cur[i]>phi_d):#(pos_d_hat[0] > 0) and (pos_d_hat[1] < 0) and (agent_r[0,i] > 0) and (self.phi_new[i]>phi_d)
-            #     ic(np.rad2deg(self.phi_cur[i]),np.rad2deg(phi_d))
-            #     self.Rot_des[:,:,i] = np.eye(3)
-            #     self.Rot_act[:,:,i] = np.eye(3)
-            # else:
-            #     if np.rad2deg(self.phi_cur[i]) < np.rad2deg(self.initial_phase[i]) and np.rad2deg(phi_d) > np.rad2deg(self.initial_phase[i]):
-            #         ic(np.rad2deg(self.phi_cur[i]),np.rad2deg(phi_d))
-            #         self.Rot_des[:,:,i] = np.eye(3)
-            #         self.Rot_act[:,:,i] = np.eye(3)
-            
-            # if np.rad2deg(self.phi_cur[i]) < 180 and np.rad2deg(phi_d) > 180:
-            #     ic(np.rad2deg(self.phi_cur[i]),np.rad2deg(phi_d))
-            #     self.Rot_des[:,:,i] = np.eye(3)
-            #     self.Rot_act[:,:,i] = np.eye(3)
 
-            # else:
-            #     self.Rot_des[:,:,i] = np.eye(3)
 
             #self.Rot_des[:,:,i] = Rot_x@Rot_y@self.Rot_act[:,:,i].copy()
-            self.Rot_des[:,:,i] = self.Rot_act[:,:,i].copy()@Rot_x.copy()
+            self.Rot_des[:,:,i] = Rot_x@self.Rot_act[:,:,i].copy()
             Rot = self.Rot_des[:,:,i]#@Rot_y@Rot_z
-            # if np.abs(phi_d - np.pi/2) < 0.001 and phi_d < np.pi:
-            #     ic(np.rad2deg(np.linalg.norm(v_d_hat_x)))
-            #     #ic(np.cos(phi_d),np.sin(phi_d))
-            #     ic(phi_dot_x)
-            #     ic(phi_d)
-            #     ic(counter)
-            # x = self.r * np.cos(phi_i)
-            # y = self.r * np.sin(phi_i)
-            # pos_d_hat = np.array([x, y, 0])
-            # pos_d = Rot@Rot_z@pos_d_hat.T
-            aux = self.Rot_des[:,:,i] - np.eye(3)
-            # if np.linalg.det(aux[1:,1:]) < 0.000001:
-            #     ic(np.linalg.det(aux[1:,1:]))
-            #     ic(counter)
-            #     ic(phi_d)
-            # if self.pass_zero[i]:
-            #     ic(np.linalg.det(aux[1:,1:]))
-            #     ic(counter,phi_d)
-            #     ic(phi_dot_x)
-            #     ic(np.rad2deg(np.arccos(self.Rot_act[1,1,i])))
-            #     # ic(self.Rot_act[:,:,i])
-            #     # ic(self.Rot_des[:,:,i])
-                # self.pass_zero[i] = False
-            # if np.abs(np.rad2deg(phi_d) - 90) < 0.7:
-            #     ic(counter)
-            #     ic(np.rad2deg(phi_d))
-            #     ic(-phi_dot_x)
-            #     ic(self.Rot_des[1:,1:,i])
-            
-            # pos_d = Rot@pos_d_hat.T
-            pos_d = Rot@pos_d_hat.T
-            if i > 0:
-                pos_d = (self.Rot_des[:,:,i].T@self.Rot_des[:,:,0]@pos_d)
+
+            if (((np.pi)-self.phi_des[i]) <0) and (((np.pi)-phi_d) >0):# and (wd > 0):
+                self.Rot_act[:,:,i] = np.eye(3)
+                self.Rot_des[:,:,i] = np.eye(3)
+                ic(self.phi_des[i],phi_d)
+            if ((self.phi_des[i]) <0.01) and (np.abs((2*np.pi)-phi_d) <0.01) and (wd > 0):
+                self.Rot_act[:,:,i] = np.eye(3)
+                self.Rot_des[:,:,i] = np.eye(3)
+                ic(self.phi_des[i],phi_d)
+            pos_d = Rot@pos_d_hat
+            # if i == 1 and not self.pass_ref[i]:
+
+            #pos_d = (self.Rot_des[:,:,0].T@pos_d)
             target_r[0, i] = pos_d[0] #+ self.phi_dot*np.cos(phi_i)*np.sin(phi_i)
             target_r[1, i] = pos_d[1] #+ self.r*np.cos(phi_i)**2
             target_r[2, i] = pos_d[2] + 0.6
             # if self.tactic == 'circle':
             #     target_r[2,i] = 0.5
-            unit[i, :] = [np.cos(phi_i), np.sin(phi_i), 0]
+            unit[i, :] = [np.cos(phi_d), np.sin(phi_d), 0]
             self.timer += 1
-
+            self.phi_des[i] = phi_d
+        # with warnings.catch_warnings(record=True) as w:
+        #     warnings.simplefilter("always")  # Ensure all warnings are captured
         k = 0
         for i in range(self.n):
             for j in range(i+1, self.n):
                 distances[k] = np.linalg.norm(target_r[:, i] - target_r[:, j])
                 phi_diff[k] = np.arccos(np.dot(unit[i,:],unit[j,:]))
                 k += 1
-        self.phi_des[i] = phi_d
+        # if len(w) > 0:
+        #     ic(unit[i,:],unit[j,:],np.dot(unit[i,:],unit[j,:]))
+        
             
         return  self.phi_cur,target_r, target_v, phi_diff, distances, debug
     
 
-    def phi_dot_desired(self,phi_i, phi_j, phi_k, phi_dot_des, k):
+    def phi_dot_desired(self,phi_i, phi_j, phi_k, phi_dot_des, k,i):
+
         phi_ki = np.mod(phi_i - phi_k, 2*np.pi)
         phi_ij = np.mod(phi_j - phi_i, 2*np.pi)
-        #ic(phi_dot_des,np.rad2deg(phi_i), np.rad2deg(phi_j),np.rad2deg(phi_k))
         return (3 * phi_dot_des + k * (phi_ki - phi_ij)) / 3
 
 
--- crazy_encirclement/encirclement_sim.py
+++ crazy_encirclement/encirclement_sim.py
@@ -11,12 +11,12 @@ from icecream import ic
 import pandas as pd
 import os
 
-N = 4000
+N = 20000
 r = 1
-k_phi = 5
+k_phi = 6
 kx = 20
 kv = 2.5*np.sqrt(2)
-n_agents = 2
+n_agents = 4
 phi_dot = 0.5#np.deg2rad(35)
 dt = 0.01
 save = False
@@ -54,9 +54,10 @@ f_T_r = np.zeros((n_agents,N))
 angles = np.zeros((3,n_agents,N))
 Wr_r = np.zeros((3,n_agents,N))
 
-agents_r[:, 0, 0] = 1*np.array([r*np.cos(0),r*np.sin(0),0.6]).T
-agents_r[:, 1, 0] = 1*np.array([r*np.cos(np.deg2rad(270)),r*np.sin(np.deg2rad(270)),0.6]).T
-# agents_r[:, 2, 0] = 1.1*np.array([r*np.cos(np.deg2rad(20)),r*np.sin(np.deg2rad(20)) ,0.6]).T
+agents_r[:, 0, 0] = 1*np.array([r*np.cos(np.deg2rad(30)),r*np.sin(np.deg2rad(30)),0.6]).T
+agents_r[:, 1, 0] = 1*np.array([r*np.cos(np.deg2rad(110)),r*np.sin(np.deg2rad(110)),0.6]).T
+agents_r[:, 2, 0] = 1.*np.array([r*np.cos(np.deg2rad(240)),r*np.sin(np.deg2rad(240)) ,0.6]).T
+agents_r[:, 3, 0] = 1.*np.array([r*np.cos(np.deg2rad(290)),r*np.sin(np.deg2rad(290)) ,0.6]).T
 
 ra_r[:,:,0] = agents_r[:,:,0]
 for i in range(n_agents):
@@ -64,7 +65,7 @@ for i in range(n_agents):
 
 embedding = Embedding(r, phi_dot,k_phi, 'dumbbell',n_agents,agents_r[:,:,0],dt)
 
-for i in range(N-1):
+for i in range(0,N-1):
     #print("percentage: ", float(i/N))
 
     phi_new, target_r_new, target_v_new, phi_diff_new, distances_new,debug = embedding.targets(agents_r[:,:,i],i)
@@ -82,10 +83,10 @@ for i in range(N-1):
     #ic(va_r[:,:,i+1])
 
 
-    # accels[:,:,i] =  kx*(ra_r[:,:,i+1] - agents_r[:,:,i]) + kv*(va_r[:,:,i+1] - agents_v[:,:,i]) # +
-    # agents_v[:,:,i+1] = agents_v[:,:,i] + accels[:,:,i]*dt #*np.random.uniform(0.2,1.2)
-    # agents_r[:,:,i+1] = agents_r[:,:,i] + agents_v[:,:,i]*dt + 0.5*accels[:,:,i]*dt**2#*np.random.uniform(0.2,1.2)
-    agents_r[:,:,i+1] = target_r_new
+    accels[:,:,i] =  kx*(ra_r[:,:,i+1] - agents_r[:,:,i]) + kv*(va_r[:,:,i+1] - agents_v[:,:,i]) # +
+    agents_v[:,:,i+1] = agents_v[:,:,i] + accels[:,:,i]*dt #*np.random.uniform(0.2,1.2)
+    agents_r[:,:,i+1] = agents_r[:,:,i] + agents_v[:,:,i]*dt + 0.5*accels[:,:,i]*dt**2#*np.random.uniform(0.2,1.2)
+    #agents_r[:,:,i+1] = target_r_new
 
 figures_dir = "figures/"
 os.makedirs(figures_dir, exist_ok=True)
@@ -96,14 +97,17 @@ ax = fig.add_subplot(111, projection='3d')
 legends = []
 for agent in range(n_agents):
     color = colors[agent]
-    ax.plot3D(ra_r[0,agent,1:-1], ra_r[1,agent,1:-1], ra_r[2,agent,1:-1],color=color)
+    ax.plot3D(ra_r[0,agent,0:-1], ra_r[1,agent,0:-1], ra_r[2,agent,0:-1],color=color,label=f"Desired trajectory agent {agent+1}")
+    ax.scatter(agents_r[0,agent,0], agents_r[1,agent,0], agents_r[2,agent,0],color=color,marker='o')
+    ax.scatter(agents_r[0,agent,-1], agents_r[1,agent,-1], agents_r[2,agent,-1],color='black',marker='o')
     #ax.plot3D(agents_r[0,agent,1:-1], agents_r[1,agent,1:-1], agents_r[2,agent,1:-1],color=color, linestyle='dashed')
-    legends.append(f"Desired trajectory agent {agent+1}")
-    legends.append(f"Real trajectory agent {agent+1}")
-ax.legend(legends)#, bbox_to_anchor=(1.05, 0.5), loc='center left', borderaxespad=0.)
+
+    #legends.append(f"Real trajectory agent {agent+1}")
+ax.legend()#, bbox_to_anchor=(1.05, 0.5), loc='center left', borderaxespad=0.)
 ax.set_xlabel('X Axis')
 ax.set_ylabel('Y Axis')
 ax.set_zlabel('Z Axis')
+#ax.view_init(elev=90, azim=-90)
 
 if save:
 # # List of perspectives to save (elev, azim)
@@ -140,62 +144,72 @@ else:
 # ax.set_ylabel('Y Axis')
 # ax.set_zlabel('Z Axis')
 # plt.savefig()
-for i in range(n_agents):
-    plt.subplot(3,1,1)
-    plt.title(f"Velocities agent {i+1}")
-    plt.plot(t[0:-1],va_r[0,i,0:-1])
-    plt.ylabel("$v_x$ (m/s)")
-    plt.subplot(3,1,2)
-    plt.plot(t[0:-1],va_r[1,i,0:-1])
-    plt.ylabel("$v_y$ (m/s)")
-    plt.subplot(3,1,3)
-    plt.plot(t[0:-1],va_r[2,i,0:-1])
-    plt.ylabel("$v_z$ (m/s)")
-    plt.xlabel("Time (s)")
+# for i in range(n_agents):
+#     plt.subplot(3,1,1)
+#     plt.title(f"Velocities agent {i+1}")
+#     plt.plot(t[0:-1],va_r[0,i,0:-1])
+#     plt.ylabel("$v_x$ (m/s)")
+#     plt.subplot(3,1,2)
+#     plt.plot(t[0:-1],va_r[1,i,0:-1])
+#     plt.ylabel("$v_y$ (m/s)")
+#     plt.subplot(3,1,3)
+#     plt.plot(t[0:-1],va_r[2,i,0:-1])
+#     plt.ylabel("$v_z$ (m/s)")
+#     plt.xlabel("Time (s)")
     
-    #plt.show()
-    if save:
-        plt.savefig(f"{figures_dir}/velocities_agent_{i+1}.png")
-        plt.close()
-    else:
-        plt.show()
+#     #plt.show()
+#     if save:
+#         plt.savefig(f"{figures_dir}/velocities_agent_{i+1}.png")
+#         plt.close()
+#     else:
+#         plt.show()
 
+# for i in range(n_agents):
+#     plt.subplot(3,1,1)
+#     plt.title(f"Positions agent {i+1}")
+#     plt.plot(t[0:-1],ra_r[0,i,0:-1])
+#     plt.plot(t[0:-1],agents_r[0,i,0:-1],linestyle='dashed')
+#     plt.legend(["Desired","Real"])
+#     plt.ylabel("x (m)")
+#     plt.subplot(3,1,2)
+#     plt.plot(t[0:-1],ra_r[1,i,0:-1])
+#     plt.plot(t[0:-1],agents_r[1,i,0:-1],linestyle='dashed')
+#     plt.legend(["Desired","Real"])
+#     plt.ylabel("y (m)")
+#     plt.subplot(3,1,3)
+#     plt.plot(t[0:-1],ra_r[2,i,0:-1])
+#     plt.plot(t[0:-1],agents_r[2,i,0:-1],linestyle='dashed')
+#     plt.legend(["Desired","Real"])
+#     plt.ylabel("z (m)")
+#     plt.xlabel("Time (s)")
+
+#     if save:
+#         plt.savefig(f"{figures_dir}/positions_agent_{i+1}.png")
+#         plt.close()
+#     else:
+#         plt.show()
 for i in range(n_agents):
-    plt.subplot(3,1,1)
-    plt.title(f"Positions agent {i+1}")
-    plt.plot(t[0:-1],ra_r[0,i,0:-1])
-    plt.plot(t[0:-1],agents_r[0,i,0:-1],linestyle='dashed')
-    plt.legend(["Desired","Real"])
-    plt.ylabel("x (m)")
-    plt.subplot(3,1,2)
-    plt.plot(t[0:-1],ra_r[1,i,0:-1])
-    plt.plot(t[0:-1],agents_r[1,i,0:-1],linestyle='dashed')
-    plt.legend(["Desired","Real"])
-    plt.ylabel("y (m)")
-    plt.subplot(3,1,3)
-    plt.plot(t[0:-1],ra_r[2,i,0:-1])
-    plt.plot(t[0:-1],agents_r[2,i,0:-1],linestyle='dashed')
-    plt.legend(["Desired","Real"])
-    plt.ylabel("z (m)")
-    plt.xlabel("Time (s)")
-
-    if save:
-        plt.savefig(f"{figures_dir}/positions_agent_{i+1}.png")
-        plt.close()
-    else:
-        plt.show()
-
-for i in range(n_diff):
-    plt.plot(t[0:-1],distances[i,0:-1],label=f"Distance agent {i+1}")
-plt.ylabel("Distances (m)")
+    plt.plot(t[0:-1],np.rad2deg(phi_cur[i,0:-1]),label=f"Angle agent {i+1}")
+plt.ylabel("Angles (degrees)")
 plt.xlabel("Time (s)")
-plt.title("Distances between agents")
+plt.title("Angles of the agents")
 plt.legend()
 if save:
-    plt.savefig(f"{figures_dir}/distances.png")
+    plt.savefig(f"{figures_dir}/angles.png")
     plt.close()
 else:
     plt.show()
+# for i in range(n_diff):
+#     plt.plot(t[0:-1],distances[i,0:-1],label=f"Distance agent {i+1}")
+# plt.ylabel("Distances (m)")
+# plt.xlabel("Time (s)")
+# plt.title("Distances between agents")
+# plt.legend()
+# if save:
+#     plt.savefig(f"{figures_dir}/distances.png")
+#     plt.close()
+# else:
+#     plt.show()
 
 for i in range(n_diff):
     plt.plot(t[0:-1],np.rad2deg(phi_diff[i,0:-1]),label=f"Phase difference agent {i+1}")
